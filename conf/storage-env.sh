0d330ff266d2b6f6e8a9001ff1861be1fda20d14
Anurag Khandelwal <anuragk@berkeley.edu>
2015-07-10 19:34:32 -0700

    Fixed scripts

--- conf/storage-env.sh
+++ conf/storage-env.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
 export HADOOP_CONF_DIR="$HADOOP_HOME/conf"
 export HADOOP_LIB_DIR="$HADOOP_HOME/lib"
-export CLASSPATH=$CLASSPATH:$HADOOP_LIB_DIR
+for JAR in $HADOOP_LIB_DIR/*.jar; do
+    export CLASSPATH=$JAR:$CLASSPATH
+done
